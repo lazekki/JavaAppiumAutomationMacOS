099637c5f24ca91126231121c91407e81d731207
lazekki <lazekkian@gmail.com>
2020-07-07 17:00:43 +0300

    III. Complex Appium tests. Homework Ex5. Test saveTwoArticlesToMyListThenDeleteOneAndCheckLast().

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -323,13 +323,15 @@ public class FirstTest {
     }
 
     @Test
-    public void saveTwoArticlesToMyList() {
+    public void saveTwoArticlesToMyListThenDeleteOneAndCheckLast() {
+        //find the search field
         waitForElementAndClick(
                 By.xpath("//*[contains(@text,'Search Wikipedia')]"),
                 "Cannot find 'Search Wikipedia' input",
                 5
         );
 
+        //type 'Java'
         waitForElementAndSendKeys(
                 By.xpath("//*[contains(@text,'Search…')]"),
                 "Java",
@@ -337,6 +339,7 @@ public class FirstTest {
                 5
         );
 
+        //open as first article the topic about Java (Object-oriented programming language)
         String first_article_xpath_text = "Object-oriented programming language";
         waitForElementAndClick(
                 By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='" + first_article_xpath_text + "']"),
@@ -344,50 +347,80 @@ public class FirstTest {
                 5
         );
 
+        //check if first article is presented
         waitForElementPresent(
                 By.id("org.wikipedia:id/view_page_title_text"),
                 "Cannot find article title",
-                15
+                5
         );
 
+        //open More options
         waitForElementAndClick(
                 By.xpath("//android.widget.ImageView[@content-desc='More options']"),
                 "Cannot find button to open More article options",
                 5
         );
 
+        //click Add to reading list
         waitForElementAndClick(
                 By.xpath("//*[@text='Add to reading list']"),
                 "Cannot find option to add article to reading list",
                 5
         );
 
+        //click Got it on overlay tip
         waitForElementAndClick(
                 By.id("org.wikipedia:id/onboarding_button"),
                 "Cannot find 'Got it' tip overlay",
                 5
         );
 
+        //clean the name for the new reading list
         waitForElementAndClear(
                 By.id("org.wikipedia:id/text_input"),
                 "Cannot find input to set name of articles folder",
                 5
         );
 
+        //set the name for the new reading list
         String name_of_folder = "Java to read";
         waitForElementAndSendKeys(
                 By.id("org.wikipedia:id/text_input"),
                 name_of_folder,
                 "Cannot put text into articles folder input",
-                5
+                20
         );
 
+        //confirm that first article will be added to this new reading list
         waitForElementAndClick(
                 By.xpath("//*[@text='OK']"),
                 "Cannot press OK button to save an article name to reading list",
                 5
         );
 
+        //close article to return back to search
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cannot close article, cannot find X link",
+                5
+        );
+
+        //check that search input field is available
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia' input",
+                5
+        );
+
+        //repeat the search one more time
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "Java",
+                "Cannot find search input",
+                5
+        );
+
+        //open as second article the topic about Java (Set of several computer software products and specifications)
         String second_article_xpath_text = "Set of several computer software products and specifications";
         waitForElementAndClick(
                 By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='" + second_article_xpath_text + "']"),
@@ -395,6 +428,82 @@ public class FirstTest {
                 5
         );
 
+        //collect second article title as element
+        WebElement second_article = waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find article title",
+                15
+        );
+
+        //collect second article title
+        String second_article_title = second_article.getAttribute("text");
+
+        //open More options
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc='More options']"),
+                "Cannot find button to open More article options",
+                5
+        );
+
+        //click Add to reading list
+        waitForElementAndClick(
+                By.xpath("//*[@text='Add to reading list']"),
+                "Cannot find option to add article to reading list",
+                5
+        );
+
+        //select already prepared reading list Java to read and click on it to save there second article
+        waitForElementAndClick(
+                By.xpath("//*[@text='Java to read']"),
+                "Cannot find a list with name 'Java to read'",
+                5
+        );
+
+        //navigate back to search
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cannot close article, cannot find X link",
+                5
+        );
+
+        //open My lists
+        waitForElementAndClick(
+                By.xpath("//android.widget.FrameLayout[@content-desc=\"My lists\"]/android.widget.ImageView"),
+                "Cannot find My lists element",
+                5
+        );
+
+        //select already prepared Java to read reading list
+        waitForElementAndClick(
+                By.xpath("//*[@text='Java to read']"),
+                "Cannot find my 'Java - to read' reading list",
+                5
+        );
+
+        //select More options for the first article in Java to read reading list
+        waitForElementAndClick(
+                By.xpath("(//android.widget.ImageView[@content-desc=\"More options\"])[2]"),
+                "Cannot find 'More options' for the reading list",
+                5
+        );
+
+        //select remove from the list option for first article
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/reading_list_item_remove_text"),
+                "Cannot find item to remove link from the list",
+                5
+        );
+
+
+        //collect last article into element
+        WebElement article_in_list = waitForElementAndClick(
+            By.xpath("//*[@text='Java (software platform)']"),
+                    "Cannot find second article in list",
+                    5
+        );
+
+        //assure that this article is second article
+        Assert.assertEquals(second_article_title, article_in_list.getAttribute("text"));
 
     }
 
