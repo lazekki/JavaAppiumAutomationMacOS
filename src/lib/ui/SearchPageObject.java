58c0caa31b3b63de028e38c71c10d62ab88e2aa4
lazekki <lazekkian@gmail.com>
2020-07-08 18:05:44 +0300

    IV. Refactoring. 04. More test cases.

--- src/lib/ui/SearchPageObject.java
+++ src/lib/ui/SearchPageObject.java
@@ -8,6 +8,7 @@ public class SearchPageObject extends MainPageObject {
     private static final String
         SEARCH_INIT_ELEMENT = "//*[contains(@text,'Search Wikipedia')]",
         SEARCH_INPUT = "//*[contains(@text,'Search…')]",
+        SEARCH_CANCEL_BUTTON = "org.wikipedia:id/search_close_btn",
         SEARCH_RESULT_BY_SUBSTRING_TPL = "//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='{SUBSTRING}']";
 
     public SearchPageObject(AppiumDriver driver) {
@@ -28,6 +29,27 @@ public class SearchPageObject extends MainPageObject {
 
         this.waitForElementPresent(By.xpath(SEARCH_INIT_ELEMENT),
                 "Cannot find search input after cicking search init element",
+                5
+        );
+    }
+
+    public void waitForCancelButtonToAppear() {
+        this.waitForElementPresent(By.id(SEARCH_CANCEL_BUTTON),
+                "Cannot find search cancel button",
+                5
+                );
+    }
+
+    public void waitForCancelButtonToDisappear() {
+        this.waitForElementNotPresent(By.id(SEARCH_CANCEL_BUTTON),
+                "Search cancel button is still present",
+                5
+        );
+    }
+
+    public void clickCancelSearch() {
+        this.waitForElementAndClick(By.id(SEARCH_CANCEL_BUTTON),
+                "Cannot find and click search cancel button",
                 5
         );
     }
