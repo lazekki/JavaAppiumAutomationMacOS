e0af97730fafd73c237cfb1ee0fe85b8b59c3b02
lazekki <lazekkian@gmail.com>
2020-07-20 10:53:07 +0300

    V. Refactoring. 05. Platforms switch. New method to create driver getDriverByPlatformEnv(DesiredCapabilities capabilities) ; Global String platform;

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -2,34 +2,44 @@ package lib;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
 import junit.framework.TestCase;
 import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
 public class CoreTestCase extends TestCase {
+
+    private static final String
+        PLATFORM_IOS = "ios",
+        PLATFORM_ANDROID = "android";
+
     protected AppiumDriver driver;
     private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
+    private static String platform = System.getenv("PLATFORM");
 
     @Override
     protected void setUp() throws Exception {
 
         super.setUp();
+        DesiredCapabilities capabilities = this.getCapabilitiesByPlatformEnv();
+        AppiumDriver driver = this.getDriverByPlatformEnv(capabilities);
+        this.rotateScreenPortrait();
+    }
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+    private AppiumDriver getDriverByPlatformEnv(DesiredCapabilities capabilities) throws Exception, MalformedURLException {
 
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "emulator-5554");
-        capabilities.setCapability("platformVersion", "9.0");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActivity", "main.MainActivity");
-        capabilities.setCapability("app", "/Users/olgazaharenko/Desktop/JavaAppiumAutomationMacOS/apks/org.wikipedia.apk");
-        capabilities.setCapability("orientation", "PORTRAIT");
-
-        driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+        if (platform.equals(PLATFORM_ANDROID)) {
+            driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+        } else if (platform.equals((PLATFORM_IOS))) {
+            driver = new IOSDriver(new URL(AppiumURL), capabilities);
+        } else {
+            throw new Exception("Cannot get run platform from env variable. Platform value " + platform);
+        }
+        return driver;
     }
 
     @Override
@@ -50,4 +60,26 @@ public class CoreTestCase extends TestCase {
         driver.runAppInBackground(Duration.ofSeconds(seconds));
     }
 
+    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        if (platform.equals(PLATFORM_ANDROID)) {
+            capabilities.setCapability("platformName", "Android");
+            capabilities.setCapability("deviceName", "AndroidTestDevice");
+            capabilities.setCapability("platformVersion", "9.0");
+            capabilities.setCapability("automationName", "Appium");
+            capabilities.setCapability("app.package", "org.wikipedia");
+            capabilities.setCapability("appActivity", ".main.MainActivity");
+            capabilities.setCapability("app", "/Users/olgazaharenko/Desktop/JavaAppiumAutomationMacOS/apks/org.wikipedia.apk");
+        } else if (platform.equals(PLATFORM_IOS)) {
+            capabilities.setCapability("platformName", "iOS");
+            capabilities.setCapability("deviceName", "iPhone SE");
+            capabilities.setCapability("platformVersion", "10.3");
+            capabilities.setCapability("app", "/Users/olgazaharenko/Desktop/JavaAppiumAutomationMacOS/apks/wikipedia.app");
+        } else {
+            throw new Exception("Cannot get run platform from env variable. Platform value " + platform);
+        }
+        return capabilities;
+    }
 }
