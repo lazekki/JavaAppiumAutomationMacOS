e3b1fc5b9f5497f0ceb736cb8edf8d7886a80df2
lazekki <lazekkian@gmail.com>
2020-07-23 16:24:52 +0300

    VI. Work with iOS. 02.

--- src/lib/ui/SearchPageObject.java
+++ src/lib/ui/SearchPageObject.java
@@ -3,24 +3,20 @@ package lib.ui;
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.WebElement;
 
-public class SearchPageObject extends MainPageObject {
+abstract public class SearchPageObject extends MainPageObject {
 
-    private static final String
-        SEARCH_INIT_ELEMENT = "xpath://*[contains(@text,'Search Wikipedia')]",
-        SEARCH_INPUT = "xpath://*[contains(@text,'Search…')]",
-        SEARCH_INPUT_ID = "id:org.wikipedia:id/search_src_text",
-        SEARCH_CANCEL_BUTTON = "id:org.wikipedia:id/search_close_btn",
-        SEARCH_RESULT_BY_SUBSTRING_TPL = "xpath://*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='{SUBSTRING}']",
-        SEARCH_RESULT_ELEMENT = "xpath://*[@resource-id='org.wikipedia:id/page_list_item_container']",
-        SEARCH_EMPTY_RESULT_ELEMENT = "xpath://*[@text='No results found']",
-        ARTICLE_TITLE_ID = "id:org.wikipedia:id/view_page_title_text",
-        ARTICLE_TITLE_XPATH = "xpath://*[@text='Java (software platform)']",
-        SEARCH_RESULT_ARTICLE_TITLE = "xpath://*[@resource-id='org.wikipedia:id/view_page_title_text']//*[@text='Java (programming language)']",
-        SEARCH_RESULT_ARTICLE_ITEM_CONTAINER_TPL =
-                "xpath:"
-              + "//*[@resource-id='org.wikipedia:id/search_container']"
-              + "//*[@resource-id='org.wikipedia:id/page_list_item_container']"
-              + "//*[@text='{TITLE_SUBSTRING}' or @text='{DESCRIPTION_SUBSTRING}']";
+    protected static String
+        SEARCH_INIT_ELEMENT,
+        SEARCH_INPUT,
+        SEARCH_INPUT_ID,
+        SEARCH_CANCEL_BUTTON,
+        SEARCH_RESULT_BY_SUBSTRING_TPL,
+        SEARCH_RESULT_ELEMENT,
+        SEARCH_EMPTY_RESULT_ELEMENT,
+        ARTICLE_TITLE_ID,
+        ARTICLE_TITLE_XPATH,
+        SEARCH_RESULT_ARTICLE_TITLE,
+        SEARCH_RESULT_ARTICLE_ITEM_CONTAINER_TPL;
 
     public SearchPageObject(AppiumDriver driver) {
         super(driver);
---
+++ src/lib/ui/ios/iOSSearchPageObject.java
@@ -0,0 +1,29 @@
+package lib.ui.ios;
+
+import io.appium.java_client.AppiumDriver;
+import lib.ui.SearchPageObject;
+
+public class iOSSearchPageObject extends SearchPageObject {
+
+    static {
+        SEARCH_INIT_ELEMENT = "xpath://XCUIElementTypeSearchField[@name='Search Wikipedia']";
+        SEARCH_INPUT = "xpath://XCUIElementTypeSearchField[@value='Search Wikipedia']";
+        //SEARCH_INPUT_ID = "id:org.wikipedia:id/search_src_text";
+        SEARCH_CANCEL_BUTTON = "id:Close";
+        SEARCH_RESULT_BY_SUBSTRING_TPL = "xpath://XCUIElementTypeLink[contains(@name, '{SUBSTRING}')]";
+        SEARCH_RESULT_ELEMENT = "xpath://XCUIElementTypeLink";
+        SEARCH_EMPTY_RESULT_ELEMENT = "xpath://XCUIElementTypeStaticText[@name='No results found']";
+        //ARTICLE_TITLE_ID = "id:org.wikipedia:id/view_page_title_text";
+        //ARTICLE_TITLE_XPATH = "xpath://*[@text='Java (software platform)']";
+        //SEARCH_RESULT_ARTICLE_TITLE = "xpath://*[@resource-id='org.wikipedia:id/view_page_title_text']//*[@text='Java (programming language)']";
+        //SEARCH_RESULT_ARTICLE_ITEM_CONTAINER_TPL =
+        //        "xpath:"
+        //                + "//*[@resource-id='org.wikipedia:id/search_container']"
+        //                + "//*[@resource-id='org.wikipedia:id/page_list_item_container']"
+        //                + "//*[@text='{TITLE_SUBSTRING}' or @text='{DESCRIPTION_SUBSTRING}']";
+    }
+
+    public iOSSearchPageObject(AppiumDriver driver) {
+        super(driver);
+    }
+}
