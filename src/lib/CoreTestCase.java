716f7c7f3c283a6a7ecf81c3e811bb618423851a
lazekki <lazekkian@gmail.com>
2020-07-23 10:53:17 +0300

    VI. Work with iOS. 01.

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -1,45 +1,22 @@
 package lib;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.ios.IOSDriver;
 import junit.framework.TestCase;
 import org.openqa.selenium.ScreenOrientation;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.time.Duration;
 
 public class CoreTestCase extends TestCase {
 
-    private static final String
-        PLATFORM_IOS = "ios",
-        PLATFORM_ANDROID = "android";
-
     protected AppiumDriver driver;
-    private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
-    private static String platform = System.getenv("PLATFORM");
+    protected Platform Platform;
 
     @Override
     protected void setUp() throws Exception {
-
         super.setUp();
-        DesiredCapabilities capabilities = this.getCapabilitiesByPlatformEnv();
-        AppiumDriver driver = this.getDriverByPlatformEnv(capabilities);
+        this.Platform = new Platform();
+        driver = this.Platform.getDriver();
         this.rotateScreenPortrait();
-    }
-
-    private AppiumDriver getDriverByPlatformEnv(DesiredCapabilities capabilities) throws Exception, MalformedURLException {
-
-        if (platform.equals(PLATFORM_ANDROID)) {
-            driver = new AndroidDriver(new URL(AppiumURL), capabilities);
-        } else if (platform.equals((PLATFORM_IOS))) {
-            driver = new IOSDriver(new URL(AppiumURL), capabilities);
-        } else {
-            throw new Exception("Cannot get run platform from env variable. Platform value " + platform);
-        }
-        return driver;
     }
 
     @Override
@@ -58,28 +35,5 @@ public class CoreTestCase extends TestCase {
 
     protected void backgroundApp(int seconds) {
         driver.runAppInBackground(Duration.ofSeconds(seconds));
-    }
-
-    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception {
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        if (platform.equals(PLATFORM_ANDROID)) {
-            capabilities.setCapability("platformName", "Android");
-            capabilities.setCapability("deviceName", "AndroidTestDevice");
-            capabilities.setCapability("platformVersion", "9.0");
-            capabilities.setCapability("automationName", "Appium");
-            capabilities.setCapability("app.package", "org.wikipedia");
-            capabilities.setCapability("appActivity", ".main.MainActivity");
-            capabilities.setCapability("app", "/Users/olgazaharenko/Desktop/JavaAppiumAutomationMacOS/apks/org.wikipedia.apk");
-        } else if (platform.equals(PLATFORM_IOS)) {
-            capabilities.setCapability("platformName", "iOS");
-            capabilities.setCapability("deviceName", "iPhone SE");
-            capabilities.setCapability("platformVersion", "10.3");
-            capabilities.setCapability("app", "/Users/olgazaharenko/Desktop/JavaAppiumAutomationMacOS/apks/wikipedia.app");
-        } else {
-            throw new Exception("Cannot get run platform from env variable. Platform value " + platform);
-        }
-        return capabilities;
     }
 }
